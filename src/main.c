d206039fc8b08cbd062ef10cd1e873a8bb5d788d
Olivier Herrou <oli.herrou@gmail.com>
2022-03-13 14:13:45 +0100

    test font

--- src/main.c
+++ src/main.c
@@ -2,12 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <SDL_ttf.h>
+
 int main(int argc, char **argv)
 {
     SDL_Init(SDL_INIT_EVERYTHING);
-
+    TTF_Init();
     SDL_Window *window = SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+    TTF_Font *Sans = TTF_OpenFont("./font/Calibri Regular.ttf", 12);
+
+    SDL_Color White = {255, 255, 255};
+
+    SDL_Surface *surfaceMessage =
+        TTF_RenderText_Solid(Sans, "HOP HOP HOP", White);
+
+    // now you can convert it into a texture
+    SDL_Texture *Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+
+    SDL_Rect Message_rect; // create a rect
+    Message_rect.x = 50;   // controls the rect's x coordinate
+    Message_rect.y = 50;   // controls the rect's y coordinte
+    Message_rect.w = 300;  // controls the width of the rect
+    Message_rect.h = 300;  // controls the height of the rect
 
     uint8_t isRunning = 1;
     SDL_Event event;
@@ -31,7 +49,8 @@ int main(int argc, char **argv)
         }
 
         SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
+        // SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 
         SDL_RenderPresent(renderer);
     }
